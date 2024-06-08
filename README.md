b7f50c2ab18ff15da329eba4508bc7e4b413007f
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-06-08 17:51:48 -0400

    feat: init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pycord Bot
+
+Before running the bot you will need to install all the requirements with this command:
+
+```
+python -m pip install -r requirements.txt
+```
+
+After that you can start it with
+
+```
+python bot.py
+```
---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import os
+import aiohttp
+from dotenv import load_dotenv
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
+
+@bot.slash_command(guild_ids=[891051215843098635])
+async def hello(ctx):
+    await ctx.respond("Hello!")
+
+@bot.slash_command(guild_ids=[891051215843098635])
+async def randomfact(ctx):
+    # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://uselessfacts.jsph.pl/random.json?language=en"
+        ) as request:
+            if request.status == 200:
+                data = await request.json()
+                embed = discord.Embed(description=data["text"], color=0xD75BF4)
+            else:
+                embed = discord.Embed(
+                    title="Error!",
+                    description="There is something wrong with the API, please try again later",
+                    color=0xE02B2B,
+                )
+            await ctx.respond(embed=embed)
+
+load_dotenv() # so we can easily access the TOKEN env var
+bot.run(os.getenv("TOKEN"))
