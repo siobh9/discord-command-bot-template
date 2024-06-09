b0c10cc9d1f6b8192ab103078219f5c9d5c93401
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-06-09 00:00:02 -0400

    chore: make guild_ids an env var

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
-import discord, os, aiohttp, random
+import discord, os, aiohttp, random, json
 from dotenv import load_dotenv
+
+load_dotenv() # so we can easily access env vars
+TOKEN = os.getenv("TOKEN")
+GUILD_IDS = json.loads(os.getenv("GUILD_IDS"))
 
 bot = discord.Bot()
 
@@ -9,11 +13,11 @@ bot = discord.Bot()
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(guild_ids=[891051215843098635])
+@bot.slash_command(guild_ids=GUILD_IDS)
 async def ping(ctx):
     await ctx.respond("Pong!")
 
-@bot.slash_command(guild_ids=[891051215843098635])
+@bot.slash_command(guild_ids=GUILD_IDS)
 async def gtn(ctx, guess:int):
     number = random.randint(1, 3)
     if guess == number:
@@ -21,11 +25,11 @@ async def gtn(ctx, guess:int):
     else:
         await ctx.send("Nope! Better luck next time :)")
 
-@bot.slash_command(guild_ids=[891051215843098635])
+@bot.slash_command(guild_ids=GUILD_IDS)
 async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(guild_ids=[891051215843098635])
+@bot.slash_command(guild_ids=GUILD_IDS)
 async def randomfact(ctx):
     # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
     async with aiohttp.ClientSession() as session:
@@ -43,5 +47,4 @@ async def randomfact(ctx):
                 )
             await ctx.respond(embed=embed)
 
-load_dotenv() # so we can easily access the TOKEN env var
-bot.run(os.getenv("TOKEN"))
+bot.run(TOKEN)
