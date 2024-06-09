026ab851d2503ce01d7a4b3fda5a3887a5ed8d86
Sean McCaffery <sean.mccaffery9@gmail.com>
2024-06-08 21:59:21 -0400

    chore: currently both working

--- bot.py
+++ bot.py
@@ -1,43 +1,28 @@
-import discord
-import os
-import aiohttp
+import discord, os
+from discord.ext import commands
 from dotenv import load_dotenv
 
-bot = discord.Bot()
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('members'):
-        await message.channel.send('here are all of the members!')
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 @bot.slash_command(guild_ids=[891051215843098635])
 async def hello(ctx):
     await ctx.respond("Hello!")
 
-@bot.slash_command(guild_ids=[891051215843098635])
-async def randomfact(ctx):
-    # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            "https://uselessfacts.jsph.pl/random.json?language=en"
-        ) as request:
-            if request.status == 200:
-                data = await request.json()
-                embed = discord.Embed(description=data["text"], color=0xD75BF4)
-            else:
-                embed = discord.Embed(
-                    title="Error!",
-                    description="There is something wrong with the API, please try again later",
-                    color=0xE02B2B,
-                )
-            await ctx.respond(embed=embed)
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Unknown command.")
 
 load_dotenv() # so we can easily access the TOKEN env var
 bot.run(os.getenv("TOKEN"))
